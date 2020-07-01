389364dcd6a53fe2c7842d24e59a65f345e7d583
KalloDavid93 <kallo.david93@gmail.com>
2020-07-01 12:55:53 +0200

    connection with currency API established

--- CurrencyExchange/Controllers/HomeController.cs
+++ CurrencyExchange/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CurrencyExchange.Models;
+using RestSharp;
 
 namespace CurrencyExchange.Controllers
 {
@@ -20,6 +21,10 @@ namespace CurrencyExchange.Controllers
 
         public IActionResult Index()
         {
+            var client = new RestClient("https://api.exchangeratesapi.io/history?start_at=2020-06-01&end_at=2020-07-01&base=USD");
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            ViewBag.Response = response.Content;
             return View();
         }
 
