f70f730fac966c9b071904d5d46ded9135aa0a3f
KalloDavid93 <kallo.david93@gmail.com>
2020-07-01 13:00:34 +0200

    conversion model created

---
+++ CurrencyExchange/Models/Conversion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange.Models
+{
+    public class Conversion
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string BaseCurrency { get; set; }
+        public string EndCurrency { get; set; }
+    }
+}
