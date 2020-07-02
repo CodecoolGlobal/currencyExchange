4d03484502812f80f5f7f09ec77a2f8e78944463
KalloDavid93 <kallo.david93@gmail.com>
2020-07-02 16:57:52 +0200

    created user model, controller and views, database added with users table

---
+++ CurrencyExchange/Data/CurrencyExchangeContext.cs
@@ -0,0 +1,19 @@
+﻿using CurrencyExchange.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange.Data
+{
+    public class CurrencyExchangeContext : DbContext
+    {
+        public CurrencyExchangeContext(DbContextOptions<CurrencyExchangeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- CurrencyExchange/Startup.cs
+++ CurrencyExchange/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CurrencyExchange.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CurrencyExchange
 {
@@ -24,6 +26,9 @@ namespace CurrencyExchange
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<CurrencyExchangeContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("CurrencyExchangeContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
