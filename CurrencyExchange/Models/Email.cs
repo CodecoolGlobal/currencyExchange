8dd86ff28ca4349be58610a5a0e63403ea2e043f
KalloDavid93 <kallo.david93@gmail.com>
2020-07-30 12:44:45 +0200

    email class, messageService added

---
+++ CurrencyExchange/Models/Email.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange.Models
+{
+    public class Email
+    {
+        public string From { get; set; }
+        public string RecipientEmail { get; set; }
+        public string RecipientUserName { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Date { get; set; }
+
+        public Email(string recipient, string subject, string body)
+        {
+            RecipientEmail = recipient;
+            Subject = subject ?? "";
+            Body = body ?? "";
+        }
+    }
+}
---
+++ CurrencyExchange/Services/MessageService.cs
@@ -0,0 +1,48 @@
+﻿using CurrencyExchange.Models;
+using MailKit.Net.Imap;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange.Services
+{
+    public class MessageService
+    {
+        public static ImapClient client;
+        private static string hostEmailAddress;
+        private static string hostEmailPassword;
+       
+        public MessageService()
+        {
+            client = new ImapClient();
+            Connection(hostEmailAddress, hostEmailPassword);
+        }
+
+        public static void Connection(string userName, string password)
+        {
+            client.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
+            client.Authenticate(userName, password);
+        }
+
+        public static void SendMail(Email email)
+        {
+            MimeMessage message = new MimeMessage();
+            BodyBuilder bodyBuilder = new BodyBuilder();
+            message.From.Add(new MailboxAddress("Currency Exchange", hostEmailAddress));
+            message.To.Add(new MailboxAddress(email.RecipientUserName, email.RecipientEmail));
+            message.Subject = email.Subject;
+            bodyBuilder.HtmlBody = email.Body;
+            message.Body = bodyBuilder.ToMessageBody();
+            SmtpClient client = new SmtpClient();
+            client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+            client.Connect("smtp.gmail.com", 587);
+            client.Authenticate(hostEmailAddress, hostEmailPassword);
+            client.Send(message);
+            client.Disconnect(true);
+        }
+    }
+}
